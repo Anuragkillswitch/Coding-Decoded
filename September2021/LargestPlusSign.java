b7ce8bcf5f75c776eea0272defac2ce0a50ab938
Anuragkillswitch <70265851+Anuragkillswitch@users.noreply.github.com>
2021-09-09 18:52:54 +0530

    Update LargestPlusSign.java

--- September2021/LargestPlusSign.java
+++ September2021/LargestPlusSign.java
@@ -1,86 +1,41 @@
 class Solution {
-    public int orderOfLargestPlusSign(int n, int[][] mines) {
-
-        int[][] inputGrid = new int[n][n];
-        for(int[] row: inputGrid){
-            Arrays.fill(row, 1);
-        }
-        for(int[] mine : mines){
-
-            inputGrid[mine[0]][mine[1]] =0;
-        }
-
-        int[][] lTR = new int[n][n];
-
-        for(int i=0;i<n;i++){
-            int runningSum = 0;
-            for(int j=0;j<n;j++){
-
-                if(inputGrid[i][j] == 0){
-                    runningSum = 0;
-                } else {
-                    runningSum = inputGrid[i][j]+runningSum;
-                }
-
-                lTR[i][j]  = runningSum;
+    public int orderOfLargestPlusSign(int N, int[][] mines) {
+        Set<Integer> banned = new HashSet();
+        int[][] dp = new int[N][N];
+        
+        for (int[] mine: mines)
+            banned.add(mine[0] * N + mine[1]);
+        int ans = 0, count;
+        
+        for (int r = 0; r < N; ++r) {
+            count = 0;
+            for (int c = 0; c < N; ++c) {
+                count = banned.contains(r*N + c) ? 0 : count + 1;
+                dp[r][c] = count;
+            }
+            
+            count = 0;
+            for (int c = N-1; c >= 0; --c) {
+                count = banned.contains(r*N + c) ? 0 : count + 1;
+                dp[r][c] = Math.min(dp[r][c], count);
             }
         }
-
-
-
-        int[][] rTL = new int[n][n];
-
-        for(int i=0;i<n;i++){
-            int runningSum = 0;
-            for(int j=n-1;j>=0;j--){
-                if(inputGrid[i][j] == 0){
-                    runningSum = 0;
-                } else {
-                    runningSum = inputGrid[i][j]+runningSum;
-                }
-                rTL[i][j] = runningSum;
+        
+        for (int c = 0; c < N; ++c) {
+            count = 0;
+            for (int r = 0; r < N; ++r) {
+                count = banned.contains(r*N + c) ? 0 : count + 1;
+                dp[r][c] = Math.min(dp[r][c], count);
+            }
+            
+            count = 0;
+            for (int r = N-1; r >= 0; --r) {
+                count = banned.contains(r*N + c) ? 0 : count + 1;
+                dp[r][c] = Math.min(dp[r][c], count);
+                ans = Math.max(ans, dp[r][c]);
             }
         }
-
-        int[][] tTB = new int[n][n];
-
-        for(int i=0;i<n;i++){
-            int runningSum = 0;
-            for(int j=0;j<n;j++){
-                if(inputGrid[j][i] == 0){
-                    runningSum = 0;
-                } else {
-                    runningSum = inputGrid[j][i]+runningSum;
-                }
-
-                tTB[j][i]  = runningSum;
-            }
-        }
-
-
-
-        int[][] bTT = new int[n][n];
-
-        for(int i=0;i<n;i++){
-            int runningSum = 0;
-            for(int j=n-1;j>=0;j--){
-                if(inputGrid[j][i] == 0){
-                    runningSum = 0;
-                } else {
-                    runningSum = inputGrid[j][i]+runningSum;
-                }
-
-                bTT[j][i]  = runningSum;
-            }
-        }
-        int ans=0;
-
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                int minLen = Math.min(Math.min(lTR[i][j], rTL[i][j]) , Math.min(bTT[i][j], tTB[i][j]));
-                ans = Math.max(ans, minLen);
-            }
-        }
+        
         return ans;
     }
 }
